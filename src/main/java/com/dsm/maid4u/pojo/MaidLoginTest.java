82160686cc86d316637e31ff19e8ce99f45feb14
Dileep <maid4u001@gmail.com>
2016-11-20 10:17:00 +0530

    Test pojo added
    Test pojo added

---
+++ src/main/java/com/dsm/maid4u/pojo/MaidLoginTest.java
@@ -0,0 +1,22 @@
+package com.dsm.maid4u.pojo;
+
+public class MaidLoginTest {
+	private String uname;
+	private String pwd;
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	
+	
+
+}
